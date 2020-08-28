6789aa49d91a265ffbc9d301e43fbb5f46677ca7
Sunny <ousunny91@gmail.com>
2020-08-28 19:24:22 -0400

    Let user add screenshot for action and switch to fullscreen

--- app/src/main/java/me/ousunny/donut/CreateActionActivity.kt
+++ app/src/main/java/me/ousunny/donut/CreateActionActivity.kt
@@ -1,14 +1,31 @@
 package me.ousunny.donut
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_action.*
+import java.io.IOException
 
 class CreateActionActivity : AppCompatActivity() {
+
+    private var imageBitmap: Bitmap? = null
+    private val CHOOSE_IMAGE_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_action)
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
 
         supportActionBar?.hide()
 
@@ -16,4 +33,46 @@ class CreateActionActivity : AppCompatActivity() {
 
     }
 
+    fun chooseImage(view: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        val chooser = Intent.createChooser(intent, "Choose image")
+        startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            val bitmap = tryReadBitmap(data.data)
+            
+            bitmap?.let {
+                this.imageBitmap = bitmap
+
+                btn_choose_image.visibility = View.GONE
+                iv_screenshot.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun tryReadBitmap(data: Uri?): Bitmap? {
+        return try {
+            MediaStore.Images.Media.getBitmap(contentResolver, data)
+        } catch(e: IOException) {
+            e.printStackTrace()
+            null
+        }
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+    }
 }
